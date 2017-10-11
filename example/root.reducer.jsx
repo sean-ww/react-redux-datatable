f6a5f4d1d3526d87077f4beb4ee6d389321c8896
sean <sean.wallis2@networkrail.co.uk>
2017-10-11 17:20:11 +0100

    Website generation with Couscous

--- example/root.reducer.jsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import DataTableReducer from '../src/DataTable.reducer';
-import DataTableExportReducer from '../src/DataTableExport.reducer';
-
-export default combineReducers({
-    DataTableReducer,
-    DataTableExportReducer,
-});
--- src/LoadingGif/LoadingGif.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const loadingGif = () => (
-    <div class="rollingSpinner" />
-);
-
-export default loadingGif;
