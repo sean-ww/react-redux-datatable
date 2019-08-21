3fb99abcbf9b1c92d58a4b0e95f7d060a5d8b2e1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-21 15:48:44 +0000

    Deploy sean-ww/react-redux-datatable to github.com/sean-ww/react-redux-datatable.git:gh-pages

--- demo/vendors~main.de5fa517057fed41363d.bundle.js
+++ demo/vendors~main.de5fa517057fed41363d.bundle.js
@@ -93,4 +93,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",function(){return isPlainObject})}]]);
-//# sourceMappingURL=vendors~main.e4b4ca0c2f6c64702795.bundle.js.map
+//# sourceMappingURL=vendors~main.de5fa517057fed41363d.bundle.js.map
